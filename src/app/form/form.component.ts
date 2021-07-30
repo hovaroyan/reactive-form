f110ad378425a339dd48eba2f43e816b38094428
hovaroyan <hovaroyan@gmail.com>
2021-07-31 02:39:25 +0400

    added form group inside an array

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,32 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
 
-   form= this.fb.group({
-      name: this.fb.control('',[Validators.required]),
-      lastName: this.fb.control('',[Validators.required]),
-      phoneNumber: this.fb.control('',[Validators.required, Validators.pattern("0[0-9]{8}")]),
-      email: this.fb.control('',[Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z.-]+\\.[a-z]{2,4}$")]),
-      password: this.fb.control('',[Validators.required, Validators.pattern("^(?=.{6,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$")]),
-      confirmPassword: this.fb.control('',[Validators.required]),
-  
-    })
+export class FormComponent implements OnInit {
+  form= this.fb.group({
+    name: this.fb.control('',[Validators.required]),
+    lastName: this.fb.control('',[Validators.required]),
+    phoneNumber: this.fb.control('',[Validators.required, Validators.pattern("0[0-9]{8}")]),
+    email: this.fb.control('',[Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z.-]+\\.[a-z]{2,4}$")]),
+    password: this.fb.control('',[Validators.required, Validators.pattern("^(?=.{6,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$")]),
+    confirmPassword: this.fb.control('',[Validators.required]),
+
+  });
+
+  items = this.fb.array([ this.form]);
 
   constructor(private fb: FormBuilder) { 
-    
   }
 
   ngOnInit(): void {
   }
-  
+
   onAdd() {
-    console.log(this.form.controls.name);
-    
+  const item: FormGroup = this.items.controls[0] as FormGroup;
+  this.items.push(item); 
   }
+    
 }
