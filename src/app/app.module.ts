edc1555cc73782788b97f6855bd2b3d761469c2d
hovaroyan <hovaroyan@gmail.com>
2021-07-30 19:57:13 +0400

    added form component and reusable input with validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    InputComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  form = this.fb.group({
+    name: this.fb.control('',[Validators.required]),
+    lastName: this.fb.control('',[Validators.required]),
+    phoneNumber: this.fb.control('',[Validators.required, Validators.pattern("0[0-9]{8}")]),
+    email: this.fb.control('',[Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z.-]+\\.[a-z]{2,4}$")]),
+    password: this.fb.control('',[Validators.required, Validators.pattern("^(?=.{6,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$")]),
+    confirmPassword: this.fb.control('',[Validators.required]),
+
+  })
+
+  constructor(private fb: FormBuilder) { 
+    
+  }
+
+  ngOnInit(): void {
+  }
+  
+  onSubmit() {
+    console.log(this.form.controls.email);
+    
+  }
+}
